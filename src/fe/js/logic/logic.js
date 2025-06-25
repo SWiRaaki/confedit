a8f02448850651e58f586e2d39138acadad75934
Volodymyr <bogatyrchuk.v109@gmail.com>
2025-06-25 09:17:38 +0200

    test sttuct with src

---
+++ src/fe/js/logic/logic.js
@@ -0,0 +1,15 @@
+﻿const express = require("express");
+const cors = require("cors");
+const WebSocket = require("ws");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const wsServerUrl = "ws://localhost:42069";
+let ws;
+
+// todo
+function test() { }
